c63a7708937cabe147c87f366ce4582be318920c
Amala <amala02507@gmail.com>
2025-05-24 13:27:44 +0530

    commiting

---
+++ sparkcode.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+from pyspark.sql.functions import *
+
+from pyspark.sql.types import *
+
+spark=SparkSession.builder.appName("spark").getOrCreate()
+
+df=spark.read.format("csv").option("header","true").load("path")
+
+df.show()
+df.printSchema()

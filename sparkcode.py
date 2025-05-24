b6464a7f62d01cda64c439938941875d32b05d45
Amala <amala02507@gmail.com>
2025-05-25 00:17:18 +0530

    spark session code

--- sparkcode.py
+++ sparkcode.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
 from pyspark.sql.types import *
-
+#spark entry point
 spark=SparkSession.builder.appName("spark").getOrCreate()
 
 df=spark.read.format("csv").option("header","true").load("path")
